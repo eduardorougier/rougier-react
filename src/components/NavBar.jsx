f98824f38c77e3b9e92295d823a5973abc5d908d
eduardorougier <eduardorougier@gmail.com>
2021-10-13 23:27:34 -0300

    Desafio 2 - Menu e-commerce

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+const NavBar = () => {
+    return (
+        <div className="barrita">
+            <div className="logo">
+                <h1>iCommerce</h1>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li><a href="www.mercadolibre.com.ar">Mercado Libre</a></li>
+                    <li><a href="www.facebook.com.ar">Facebook Market</a></li>
+                    <li><a href="www.amazon.com">Amazon</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
